8ec0e157744fb31059a7ee5d236a2f677f38dc39
Abhishek Tiwari <a.tiwari@digicust.com>
2023-10-23 21:35:44 +0530

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = procsess.env.port||5511; // Change to your desired port
+const port = process.env.PORT||5511; // Change to your desired port
 
 app.use(cors());
 app.use(bodyParser.json());
