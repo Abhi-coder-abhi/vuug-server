8a5cb28cffe617c967b0f3f87fb9ac3ba8f119ee
Abhishek Tiwari <a.tiwari@digicust.com>
2023-10-23 22:38:03 +0530

    commit

--- index.js
+++ index.js
@@ -77,10 +77,29 @@ app.post('/sendData', async (req, res) => {
     res.status(500).json({ error: 'User data insertion error' });
   }
 });
+app.post('/checkLogin', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Add your authentication logic here to check if the provided email and password are valid.
+  // For example, you can query the 'User' model in the database to validate the user's credentials.
+
+  User.findOne({ email, password }, (err, user) => {
+    if (err) {
+      console.error('Database query error: ' + err.message);
+      res.status(500).json({ error: 'Database query error' });
+    } else if (user) {
+      // User authentication is successful
+      res.status(200).json({ message: 'Login successful', data: user });
+    } else {
+      // Invalid email or password
+      res.status(401).json({ error: 'Invalid email or password' });
+    }
+  });
+});
 
 // Handle booking request
 app.post('/book-room', async (req, res) => {
-  const { checkin, checkout, adult, children, email } = req.body;
+  const { checkin, checkout, adult, children, email, type} = req.body;
   // Create a new booking document
   const newBooking = new Booking({
     checkin,
@@ -88,6 +107,7 @@ app.post('/book-room', async (req, res) => {
     adult,
     children,
     email,
+    type,
   });
 
   try {
